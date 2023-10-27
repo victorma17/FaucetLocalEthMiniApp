5db9906e0804673472b54941caa97814b712db73
victor.giron <victor.giron@io.builders>
2023-10-27 19:33:38 +0200

    release version 1.0

--- back/app.js
+++ back/app.js
@@ -1,50 +1,45 @@
-const { Web3 } = require("web3");
-const fs = require("fs");
-const cors = require("cors");
-const express = require("express");
+const { Web3 } = require("web3")
+const fs = require("fs")
+const cors = require("cors")
+const express = require("express")
 
-const app = express();
+const app = express()
+const web3 = new Web3("http://localhost:8545")
+const json = JSON.parse(fs.readFileSync("../nodo/data/keystore/account.json"))
 
-app.use(cors());
-
-const web3 = new Web3("http://localhost:8545");
-
-
+app.use(cors)
 app.listen(3455)
 
 app.get("/balance/:address", async (req, res) => {
-    try {
-        const balance = await web3.eth.getBalance(req.params.address);
-        res.json({ balance: balance.toString() }); // Convert BigInt to string
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Failed to retrieve balance" });
-    }
+  try {
+    const balance = await web3.eth.getBalance(req.params.address)
+    res.json({ balance: balance.toString() }) // Convert BigInt to string
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "Failed to retrieve balance" })
+  }
 })
 
-
-const json = JSON.parse(fs.readFileSync("../nodo/data/keystore/account.json"));
 // API endpoint to send ether from the faucet to the specified address
 app.get("/faucet/:address", async (req, res) => {
-    try {
-      const account = await web3.eth.accounts.decrypt(json, "1234");
-      const tx = {
-        chainId: 8888,
-        to: req.params.address,
-        from: account.address,
-        gas: 30,
-        value: web3.utils.toWei("1000", "ether")
-         ,gasPrice: 1
-         ,gasLimit: 500000
-      };
-      // signed the transaction
-      const txSigned = await account.signTransaction(tx);
-      // send the aswaer of the transaction
-      const receipt = await web3.eth.sendSignedTransaction(txSigned.rawTransaction);
-      res.json({ transactionHash: receipt.transactionHash });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: "Failed to send ether" });
+  try {
+    const account = await web3.eth.accounts.decrypt(json, "1234")
+    const tx = {
+      chainId: 8888,
+      to: req.params.address,
+      from: account.address,
+      gas: 30,
+      value: web3.utils.toWei("1000", "ether"),
+      gasPrice: 1,
+      gasLimit: 500000
     }
-  });
-
+    // signed the transaction
+    const txSigned = await account.signTransaction(tx)
+    // send the aswaer of the transaction
+    const receipt = await web3.eth.sendSignedTransaction(txSigned.rawTransaction)
+    res.json({ transactionHash: receipt.transactionHash })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "Failed to send ether" })
+  }
+})
--- readme.md
+++ readme.md
@@ -1,32 +1,68 @@
+--
+Delete data folder
+Set wallet address like empty
+--
+
+Run Docker Desktop
+Open your Metamask
+set the wallet direction in genesis.json and save
+
+
 ** ethereum/client-go **
 
+In terminal:
+
+cd nodo
 
 docker run --rm -it \
 -v ${PWD}/data/keystore:/data \
 ethereum/client-go:v1.11.0 account new \
 --keystore data
 
+// Set password: 1234 (for example)
 
 docker run --rm -it \
 -v ${PWD}/data:/data \
 -v ${PWD}/genesis.json:/genesis.json \
 ethereum/client-go:v1.11.0 init --datadir data /genesis.json
 
-docker run -d -p 8545:8545 --name ethe-node-8888 -v ${PWD}/data:/data ethereum/client-go:v1.11.0 --datadir data --http --http.api persona,admin,eth,net,web3 --http.addr 0.0.0.0 --http.port 8545 --mine --miner.etherbase 0x61f9a204cf8a62436018f0abc2c110e879330150 --http.corsdomain="*" --miner.threads 1
+// Set your miner account (Could be the new account created before or one of your metamask to see the first results)
+
+docker run -d -p 8545:8545 --name ethe-node-8888 -v ${PWD}/data:/data ethereum/client-go:v1.11.0 --datadir data --http --http.api persona,admin,eth,net,web3 --http.addr 0.0.0.0 --http.port 8545 --mine --miner.etherbase 0x0abf6d15320c366ba60bcdcc1b8e6b291d7cbf4c --http.corsdomain="*" --miner.threads 1
+
+This could take some minutes, refresh cache in metamask and you will see the balance of your miner account
 
 ** back **
 
+cd ..
+cd back
 yarn init -y
 yarn add web3
 yarn add express
-yarn add web3
 yarn add cors
+npx nodemon app.js
 
-curl localhost:3455/faucet/0x1157466D2Df9D52672e8a62E5e346e8d38B68b84
-curl localhost:3455/balance/0x61f9a204cf8a62436018f0abc2c110e879330150
+// Now we go to Repeat the previous process first
+Before, 
+cd ..
+cd nodo
+and drop the container and data folder and take the address of the new account created and replace it in the same places as you set your previous account and go to the previous folder, rename the file created (UTC--... for account.json) and take the new wallet address (add an 0x where you paste it (genesis and comand))
+
+
+//new terminal 
+// direcion of the destiny of the funds from faucet account
+curl localhost:3455/faucet/0x60f42cEa9684686bF059b0cFC015D40d99b0Bc31
+
+//Remember to refresh the cache in metamask to see the balance of the new account, could take a minutes
+
+curl localhost:3455/balance/0x72d9179482aD8bb260e606fAE92173c157F2820e
 
 ** front **
-
-yarn create vite front --template react
+cd ..
+cd front
 yarn
 yarn dev
+
+// Enjoy!
+
+```
